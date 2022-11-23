d94108d34e92d959830882b3ab488f548162f517
David <david.njoroge@student.moringaschool.com>
2022-11-23 22:07:59 +0300

    completed routing basics

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_30_181635) do
+ActiveRecord::Schema.define(version: 2022_11_23_184937) do
 
   create_table "students", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.integer "grade"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "students_controllers", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
